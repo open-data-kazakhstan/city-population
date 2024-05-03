3a9514bc47c36d0b8c3b62124719adf20fba3a1d
KurosJS <polatbek0120@gmail.com>
2024-05-04 01:44:17 +0500

    data format fix

--- scripts/transform.py
+++ scripts/transform.py
@@ -2,40 +2,43 @@ import pandas as pd
 import numpy as np
 
 # Loading the data
-df = pd.read_excel('../archive/source.xlsx', sheet_name='01')
+df = pd.read_excel("../archive/source.xlsx", sheet_name='01')
 
 # Choosing relevant columns and rows
 df.dropna(inplace=True)
 df = df.iloc[:, :4]
-df = df.iloc[1:]
 # Renaming columns
 df.columns = ['Region', 'Total', 'Males', 'Females']
 
-city_names = [
-    "Abai",
-    "Akmola",
-    "Aktobe",
-    "Almaty",
-    "Atyrau",
-    "Batys Kazakhstan",
-    "Zhambyl",
-    "Zhetisu",
-    "Karagandy",
-    "Kostanai",
-    "Kyzylorda",
-    "Mangistau",
-    "Pavlodar",
-    "Soltustik Kazakhstan",
-    "Turkistan",
-    "Ulytau",
-    "Shygys Kazakhstan",
-    "Astana city",
-    "Almaty city",
-    "Shymkent city"
-]
+name_map = {
+    "Қазақстан Республикасы ": "The Republic of Kazakhstan",
+    "Абай": "Abai Region",
+    "Ақмола": "Akmola Region",
+    "Ақтөбе": "Aktobe Region",
+    "Алматы": "Almaty Region",
+    "Атырау": "Atyrau Region",
+    "Батыс Қазақстан": "West Kazakhstan Region",
+    "Жамбыл": "Jambyl Region",
+    "Жетісу": "Jetisu Region",
+    "Қарағанды": "Karaganda Region",
+    "Қостанай": "Kostanay Region",
+    "Қызылорда": "Kyzylorda Region",
+    "Маңғыстау": "Mangystau Region",
+    "Павлодар": "Pavlodar Region",
+    "Солтүстік Қазақстан": "North Kazakhstan Region",
+    "Түркістан облысы": "Turkistan Region",
+    "Ұлытау": "Ulytau Region",
+    "Шығыс Қазақстан": "East Kazakhstan Region",
+    "Астана қаласы": "Astana city",
+    "Алматы қаласы": "Almaty city",
+    "Шымкент қаласы": "Shymkent city"
+}
 
-# Changing Kazakh names to English names
-df['Region'] = city_names
+df['Region'] = df['Region'].map(name_map)
+
+# Changing first row to the end of dataframe
+df = pd.concat([df.iloc[1:], df.iloc[:1]], ignore_index=True)
+
 
 # Changing data types of three columns to integer
 cols_to_convert = ['Total', 'Males', 'Females']
